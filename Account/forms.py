00620ac5f0deaca70486f9f2ee1e7d1c56e390b7
PyDev-cloud <58389280+RajuAhamed220@users.noreply.github.com>
2025-04-25 00:41:11 +0600

    Loging Add

--- Account/forms.py
+++ Account/forms.py
@@ -1,8 +1,7 @@
-from typing import Any, Mapping
 from django import forms
 from django.contrib.auth import get_user_model
 
-User=get_user_model()
+User = get_user_model()
 class LoginForm(forms.Form):
     def __init__(self,*args, **kwargs): 
         super().__init__(*args, **kwargs)
@@ -14,37 +13,32 @@ class LoginForm(forms.Form):
 
 
 
-
 class RegisterForm(forms.ModelForm):
-    password=forms.CharField(max_length=50,widget=forms.PasswordInput)
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', max_length=50, widget=forms.PasswordInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         for field in self.fields:
             self.fields[field].widget.attrs.update({"class": "form-control"})
 
-
     class Meta:
-        model=User
-        fields=(
-            "username",
-            'email'
-        )
+        model = User
+        fields = ("username", "email")
 
-    def clean_password(self,*args, **kwargs):
-        password = self.cleaned_data.get('password')
-        password2=self.data.get('password2')
-        
-        if password != password2:
-            raise forms.ValidationError("Password not mach")
-        return password
-        
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
 
-    def save(self, commit=True,*args, **kwargs):
-        user=self.instance
+        if password and password2 and password != password2:
+            self.add_error('password2', "Passwords do not match")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
         user.set_password(self.cleaned_data.get('password'))
         if commit:
             user.save()
         return user
-    
